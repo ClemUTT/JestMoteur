15d719beaa06c25eb3cdf592cb36e025d6494fc8
cleme <cleme@DESKTOP-CDO3PB8>
2020-01-17 21:32:56 +0100

    Vue et ImageJest javadoc faits

--- JestMoteur/src/fr/thomas_clement/vue/ImageJest.java
+++ JestMoteur/src/fr/thomas_clement/vue/ImageJest.java
@@ -5,7 +5,18 @@ import java.net.URL;
 
 import javax.swing.ImageIcon;
 
+/**
+ * Classe qui est hérite de ImageIcon et qui permet de configuer une image (en l'occurence ici une carte)
+ * avec une taille par défaut
+ */
 public class ImageJest extends ImageIcon{
+	
+	/**
+	 * Constructeur de la classe ImageJest prenant l'URL de l'image et lui définissant une taille qui peut être modifiée par la suite
+	 * 
+	 * @param url
+	 * 				prend l'URL de l'image
+	 */
 	public ImageJest(URL url) {
 		super(url);
 		super.getImage().getScaledInstance(10, 10, Image.SCALE_SMOOTH);
